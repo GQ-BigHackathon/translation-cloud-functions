6fedaab3b0d3ba6f764ee228e8f1c9131e102043
Andrew A. Barber <hello@andrewbarber.me>
2022-08-27 00:40:44 +0100

    Added storepreview endpoint

---
+++ api/integrations/bigcommerce/storePreview.ts
@@ -0,0 +1,17 @@
+import { http } from '../../../lib/helpers/http';
+import axios from 'axios';
+
+const storePreview = http(['POST'], async (req, res) => {
+  const { url } = req.body;
+  if (!url) {
+    res.status(400).send('No url specified');
+    return;
+  }
+  const response = await axios.get(url);
+  const html = await response.data;
+
+  res.status(200).send(html);
+  return;
+});
+
+export default storePreview;
