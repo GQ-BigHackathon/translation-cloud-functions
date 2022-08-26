bb287517dc4fb0cc50c184bdc4b8b25afb943aba
Andrew A. Barber <hello@andrewbarber.me>
2022-08-26 22:54:06 +0100

    storehash is lowecase

--- lib/helpers/http.ts
+++ lib/helpers/http.ts
@@ -40,10 +40,11 @@ export const http = (
     }
 
     console.log('req.headers', req.headers);
-    if (!req.headers.origin || !req.headers.referer || !req.headers.StoreHash) {
+    if (!req.headers.origin || !req.headers.referer || !req.headers.storehash) {
       res.status(400).send('Not allowed');
       return;
     }
+    console.log('passed');
 
     // const hostnameFind = await Store.findOne({
     //   hostname: req.headers.origin,
